8000f32584fdd5196690de16d3482a3de74a610f
Abdulkadir_USTA <usta1829@gmail.com>
2024-07-12 16:31:00 +0300

    ready project to deploy

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,7 @@
 export const PORT = 5555;
 
-export const mongoDBURL =
-  'mongodb+srv://root:root@books-store-mern.c7ba8z1.mongodb.net/books-collection?retryWrites=true&w=majority';
+export const mongoDBURL = 'mongodb+srv://usta1829:5ibPUoPnt101fy9O@books-store-mern.2kv5nvh.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN';
 
 // Please create a free database for yourself.
 // This database will be deleted after tutorial
+//5ibPUoPnt101fy9O
