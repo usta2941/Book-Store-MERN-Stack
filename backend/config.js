7935c0e1b2c811ae77d22642d1c0bf972ef20063
Abdülkadir USTA <61731064+usta2941@users.noreply.github.com>
2024-08-14 20:02:26 +0300

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,6 @@
 export const PORT = 5555;
 
-export const mongoDBURL = 'mongodb+srv://usta1829:5ibPUoPnt101fy9O@books-store-mern.2kv5nvh.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN';
+export const mongoDBURL = '.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN';
 
 // Please create a free database for yourself.
 // This database will be deleted after tutorial
